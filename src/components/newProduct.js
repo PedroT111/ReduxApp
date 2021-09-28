07c6554099a07a88df314039678647a6bc46c3c1
pedro <pedrochincolla11@gmail.com>
2021-09-28 18:54:40 -0300

    add products to state

--- src/components/newProduct.js
+++ src/components/newProduct.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {createNewProduct} from "../core/actions/productActions";
-import {Row, Col, Card, CardBody, Form, FormGroup, Input, Label, Button, InputGroup, InputGroupText } from "reactstrap";
+import { Row,
+         Col,
+         Card,
+         CardBody,
+         Form,
+         FormGroup,
+         Input, Label, Button, InputGroup, InputGroupText } from "reactstrap";
+import { Alert } from 'reactstrap';
 
 
-const NewProduct = () => {
-
+const NewProduct = ({history}) => {
     const[product, setProduct]= useState({
         name: "",
         price: ""
@@ -18,16 +24,21 @@ const NewProduct = () => {
     };
 
     const dispatch = useDispatch();
+
+    const loading = useSelector(state => state.products.loading);
+    const error = useSelector(state => state.products.error);
+
    const addProduct = p => dispatch(createNewProduct(p));
 
     const onSubmit = (e) => {
         e.preventDefault();
-
-
         //create product
         addProduct(product);
 
+        history.push('/');
+
     }
+
 
     return(
         <Row className="justify-content-center">
@@ -56,6 +67,12 @@ const NewProduct = () => {
                             </FormGroup>
                             <Button className="outline-secondary font-weight-bold w-100 d-block mt-4">Add Product</Button>
                         </Form>
+                        {
+                            error ? <Alert color="danger" className="mt-3">There was an Error</Alert> : null
+                        }
+                        {
+                            //loading? agregar spinner de carga
+                        }
                     </CardBody>
                 </Card>
             </Col>
--- src/core/reducers/productReducer.js
+++ src/core/reducers/productReducer.js
@@ -11,6 +11,8 @@ const initialState = {
 };
 
 
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function(state = initialState, action) {
     switch(action.type){
         case ADD_PRODUCT:{
