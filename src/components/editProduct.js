ed3d53ecf192773de8c152b0cb8b26478d8f7176
pedro <pedrochincolla11@gmail.com>
2021-09-29 12:24:17 -0300

    reacstrap

--- src/components/editProduct.js
+++ src/components/editProduct.js
@@ -1,47 +1,45 @@
 import React from "react"
-
-
-
+import { Button,
+         CardBody,
+         Container,
+         Form,
+         FormGroup,
+         Input,
+         Label,
+         Row,
+         Col,
+         Card } from "reactstrap";
 const EditProduct = () => {
     return(
-        <div className="row justify-content-center">
-            <div className="col-md-8">
-                <div className="card border border-secondary border-3">
-                    <div className="card-body">
-                        <h2 className="text-center">Edit Product</h2>
-
-                        <form>
-                            <div className="form-group">
-                            <label className="col-form-label">Product Name</label>
-                            <input 
-                            className="form-control"
-                            type="text"
-                            name="name"
-                            />
-                            </div>
-                            <div className="form-group">
-                            <label className="col-form-label">Price</label>
-                            <div class="input-group">
-                                <span class="input-group-text">$</span>
-                                <input 
-                                className="form-control"
-                                type="number"
-                                name="price"
-                                />
-                            </div>
-                            
-                            </div> 
-
-                            <button className="btn btn-outline-secondary font-weight-bold w-100 d-block mt-4">Edit Product</button>                           
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-
-
-
-}
+        <Container>
+            <Row className="justify-content-center">
+                <Col md="8">
+                    <Card className="border border-secondary border-3">
+                        <CardBody>
+                            <h2 className="text-center">Edit Product</h2>
+                            <Form>
+                                <FormGroup className="mt-4">
+                                    <Label>Product Name</Label>
+                                    <Input
+                                        type="text"
+                                        name="name"
+                                    />
+                                </FormGroup>
+                                <FormGroup className="mt-4">
+                                    <Label>Price</Label>
+                                    <Input
+                                        type="number"
+                                        name="price"
+                                    />       
+                                </FormGroup>
+                                <Button className="w-100 mt-4">Edit Product</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default EditProduct;
